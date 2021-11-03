0126210908d3f8090d8d631708ae4028fc08fc61
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2021-11-03 14:44:28 -0400

    lint fixes

--- examples/vs1053_MIDI.py
+++ examples/vs1053_MIDI.py
@@ -1,7 +1,8 @@
-# vs1053 MIDI example by Eric Rosenbaum
+# SPDX-FileCopyrightText: 2021 Eric Rosenbaum
+# SPDX-License-Identifier: MIT
 
-import board
 import time
+import board
 import busio
 
 VS1053_BANK_DEFAULT = 0x00
@@ -18,44 +19,51 @@ MIDI_CHAN_PROGRAM = 0xC0
 
 uart = busio.UART(board.TX, board.RX, baudrate=31250)
 
-def noteOn(channel, note, vel):
+
+def note_on(channel, note, vel):
     uart.write(bytearray([MIDI_NOTE_ON | channel, note, vel]))
 
-def noteOff(channel, note):
+
+def note_off(channel, note):
     uart.write(bytearray([MIDI_NOTE_OFF | channel, note, 0]))
 
-def setChannelBank(channel, bank):
+
+def set_channel_bank(channel, bank):
     uart.write(bytearray([MIDI_CHAN_MSG | channel, MIDI_CHAN_BANK, bank]))
 
-def setChannelVolume(channel, vol):
+
+def set_channel_volume(channel, vol):
     uart.write(bytearray([MIDI_CHAN_MSG | channel, MIDI_CHAN_VOLUME, vol]))
 
-def setChannelInstrument(channel, num):
+
+def set_channel_instrument(channel, num):
     uart.write(bytearray([MIDI_CHAN_PROGRAM | channel]))
     time.sleep(0.01)
     uart.write(bytearray([num]))
     time.sleep(0.01)
 
-def setChannelPan(channel, pan):
+
+def set_channel_pan(channel, pan):
     uart.write(bytearray([MIDI_CHAN_MSG | channel, MIDI_CHAN_PAN, pan]))
 
+
 # Set up a piano instrument on channel 0
-setChannelBank(0, VS1053_BANK_MELODY)
-setChannelVolume(0, 127)
-setChannelInstrument(0, 0) # piano
+set_channel_bank(0, VS1053_BANK_MELODY)
+set_channel_volume(0, 127)
+set_channel_instrument(0, 0)  # piano
 
 # Play Do Re Mi
-noteOn(0, 60, 127)
+note_on(0, 60, 127)
 time.sleep(0.5)
-noteOff(0, 60)
+note_off(0, 60)
 
-noteOn(0, 62, 127)
+note_on(0, 62, 127)
 time.sleep(0.5)
-noteOff(0, 62)
+note_off(0, 62)
 
-noteOn(0, 64, 127)
+note_on(0, 64, 127)
 time.sleep(0.5)
-noteOff(0, 64)
+note_off(0, 64)
 
 time.sleep(1)
 
@@ -64,12 +72,12 @@ time.sleep(1)
 # https://cdn-shop.adafruit.com/datasheets/vs1053.pdf
 scale = [60, 62, 64, 65, 67, 69, 71, 72]
 for instrument in range(127):
-    print('instrument: ' + str(instrument))
-    setChannelInstrument(0, instrument)
-    for note in scale:
-        noteOn(0, note, 127)
+    print("instrument: " + str(instrument))
+    set_channel_instrument(0, instrument)
+    for note_num in scale:
+        note_on(0, note_num, 127)
         time.sleep(0.1)
     time.sleep(1)
-    for note in scale:
-        noteOff(0, note)
+    for note_num in scale:
+        note_off(0, note_num)
     time.sleep(0.5)
